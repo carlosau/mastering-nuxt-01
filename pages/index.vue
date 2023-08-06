17becce449f6218ef059c3e27cab2c62d9249248
carlosau <carlosaugsz@hotmail.com>
2023-08-06 10:44:54 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <Head>
+    <Title>Mastering Nuxt 01 </Title>
+  </Head>
+  <div class="text-center mt-4">
+    <h1 class="text-green-400 text-2xl">Hello Nuxt!!!</h1>
+  </div>
+</template>
