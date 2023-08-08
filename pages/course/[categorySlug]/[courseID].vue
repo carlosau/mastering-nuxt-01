648ac8d50a373f3d5d72e6671d40102661e7d13b
carlosau <carlosaugsz@hotmail.com>
2023-08-07 23:32:28 -0300

    created courseData.js and useCourse composable

--- pages/course/[categorySlug]/[courseID].vue
+++ pages/course/[categorySlug]/[courseID].vue
@@ -2,11 +2,14 @@
 import { useRoute } from "vue-router";
 
 const route = useRoute();
+
+const course = useCourse();
 </script>
 
 <template>
   <div>
     <p>Category is: {{ $route.params.categorySlug }}</p>
     <p>Course ID is: {{ $route.params.courseID }}</p>
+    <p>{{ course.cursos[12].title }}</p>
   </div>
 </template>
